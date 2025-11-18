2e869c6d031fa333f5afaea2c7edf8706d577f87
Airton Rafael Fabre <airton.fabre@confience.io>
2025-11-18 14:30:53 -0300

    Corrections of database

--- finance_portal/settings.py
+++ finance_portal/settings.py
@@ -20,8 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+import os
+# ...
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yynb&safgtf!2$is7x+6g$bttw2(j*ams_xdmn@1awkdc!%-3w'
+SECRET_KEY = os.environ.get('SECRET_KEY') 
+
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+# Check if key is missing when debug is False
+if not SECRET_KEY and not DEBUG:
+    raise Exception("SECRET_KEY environment variable is not set!")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
